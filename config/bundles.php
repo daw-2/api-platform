b29d14ceadd8cc8d543ef690607dede3c7e977c9
Matthieu Mota <matthieumota@gmail.com>
2022-09-27 15:18:06 +0200

    Encore and React

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
